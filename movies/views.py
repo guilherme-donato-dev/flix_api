e6db856f70a62d115442ecc0291b23158f133999
Guilherme Donato <donatocruzg@gmail.com>
2025-03-12 16:33:57 -0300

    iniciado o endpoint de stats

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from movies.models import Movie
 from movies.serializers import MovieSerializer
@@ -14,3 +14,15 @@ class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+
+class MovieStatsView(views.APIView):  #esse é o tipo de view que você usa quando você quer criar a sua própria view na mão, é o mais generico possivel
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Movie.objects.all()
+    
+
+    def get(self,request):
+        return response.Response(
+            data={'message' : 'Funcionou'},
+            status=status.HTTP_200_OK,
+        )
