9422c1ee5ca2d9a485870ad9f8b34cb9ebb29470
Guilherme Donato <donatocruzg@gmail.com>
2025-03-27 22:20:41 -0300

    alterado o serializers

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,10 +1,27 @@
-from movies.models import Movie
-from rest_framework import serializers
 from django.db.models import Avg
-from genres.serializers import GenreSerializer
+from rest_framework import serializers
 from actors.serializers import ActorSerializer
+from movies.models import Movie
+from genres.models import Genre
+from genres.serializers import GenreSerializer
+from actors.models import Actor
 
-class MovieSerializer(serializers.ModelSerializer):
+
+class MovieSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField()
+    genre = serializers.PrimaryKeyRelatedField(
+        queryset=Genre.objects.all(),
+    )
+    release_date = serializers.DateField()
+    actors = serializers.PrimaryKeyRelatedField(
+        queryset=Actor.objects.all(),
+        many=True,
+    )
+    resume = serializers.CharField()
+
+
+class MovieModelSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Movie
@@ -12,28 +29,35 @@ class MovieSerializer(serializers.ModelSerializer):
 
     def validate_release_date(self, value):
         if value.year < 1900:
-            raise serializers.ValidationError('O ano de lançamento do filme não pode ser menor que 1900.')
+            raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1900.')
         return value
-    
+
     def validate_resume(self, value):
-        if value.resume > 500:
-            raise serializers.ValidationError('O resumo não pode ser maior que 250 caracteres.')
+        if len(value) > 500:
+            raise serializers.ValidationError('Resumo não deve ser maior do que 500 caracteres.')
         return value
-    
+
 
 class MovieListDetailSerializer(serializers.ModelSerializer):
     actors = ActorSerializer(many=True)
     genre = GenreSerializer()
-    rate = serializers.SerializerMethodField(read_only=True) #serializer method field é sempr eum campo calculado
+    rate = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Movie
         fields = ['id', 'title', 'genre', 'actors', 'release_date', 'rate', 'resume']
 
-    def get_rate(self, obj): #essa função é a que vai calcular o method field, ele tem o nome de rate porque o nome da variavel la no serializer é rate
-        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']  #fazendo a média das avaliações com as funcs ja prontas do django
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
 
         if rate:
-            return round(rate, 1) #retornando somente com uma casa decimal
-        
+            return round(rate, 1)
+
         return None
+
+
+class MovieStatsSerializer(serializers.Serializer):
+    total_movies = serializers.IntegerField()
+    movies_by_genre = serializers.ListField()
+    total_reviews = serializers.IntegerField()
+    average_stars = serializers.FloatField()
--- movies/views.py
+++ movies/views.py
@@ -1,47 +1,52 @@
+from django.db.models import Count, Avg
 from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
-from django.db.models import Count, Avg
-from movies.models import Movie
-from movies.serializers import MovieSerializer, MovieListDetailSerializer
 from app.permissions import GlobalDefaultPermission
+from movies.models import Movie
+from movies.serializers import MovieModelSerializer, MovieListDetailSerializer, MovieStatsSerializer
 from reviews.models import Review
 
 
 class MovieCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated, GlobalDefaultPermission)
-    queryset = Movie.objects.all()
-    
-    def get_serializer_class(self):
-        if self.request.method == 'GET':
-            return MovieListDetailSerializer
-        return MovieSerializer
-
-class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated, GlobalDefaultPermission)
-    queryset = Movie.objects.all()
-    
-    def get_serializer_class(self):
-        if self.request.method == 'GET':
-            return MovieListDetailSerializer
-        return MovieSerializer
-
-
-class MovieStatsView(views.APIView):  #esse é o tipo de view que você usa quando você quer criar a sua própria view na mão, é o mais generico possivel
     permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
-    
 
-    def get(self,request):
-        total_movies = self.queryset.count() #capturando o total de filmes
-        movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id')) #dois __ eu busco o value do que eu quero, por exemplo genre__name, eu quero so o name do genre. o annotate count vai informar quantos filmes tem de cada genre. ex.: ação: 4
-        total_reviews = Review.objects.count() #quantidade total de reviews
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return MovieListDetailSerializer
+        return MovieModelSerializer
+
+
+class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Movie.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return MovieListDetailSerializer
+        return MovieModelSerializer
+
+
+class MovieStatsView(views.APIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Movie.objects.all()
+
+    def get(self, request):
+        total_movies = self.queryset.count()
+        movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
+        total_reviews = Review.objects.count()
         average_stars = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']
 
-        return response.Response(data={
-               ' total_movies' : total_movies,
-               'movies_by_genre' : movies_by_genre,
-               'total_reviews' : total_reviews,
-               'average_stars' : round(average_stars, 1) if average_stars else 0,
-            },
+        data = {
+            'total_movies': total_movies,
+            'movies_by_genre': movies_by_genre,
+            'total_reviews': total_reviews,
+            'average_stars': round(average_stars, 1) if average_stars else 0,
+        }
+        serializer = MovieStatsSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+
+        return response.Response(
+            data=serializer.validated_data,
             status=status.HTTP_200_OK,
         )
